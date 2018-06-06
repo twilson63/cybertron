6439b20c2549eeaad601e35e1086c8c29288472e
Will Adkins <wkadki01@gmail.com>
2018-06-06 14:54:32 -0400

    Added another object focused level (levelExtra.js) and included it in the index

---
+++ levelExtra.js
@@ -0,0 +1,139 @@
+import test from 'tape'
+import { map, filter, reduce, compose } from 'nanofp'
+
+const pets = [
+  {
+    name: 'Nuggett',
+    age: 23,
+    alive: false,
+    breed: 'Golden Retriever',
+    type: 'dog'
+  },
+  {
+    name: 'Thor',
+    age: 2,
+    alive: true,
+    breed: 'German Shepherd',
+    type: 'dog'
+  },
+  {
+    name: 'Scrappy',
+    age: 22,
+    alive: false,
+    breed: 'Beagle',
+    type: 'dog'
+  },
+  {
+    name: 'Spot',
+    age: 21,
+    alive: false,
+    breed: 'Dachsund',
+    type: 'dog'
+  },
+  {
+    name: 'Shyloh',
+    age: 24,
+    alive: false,
+    breed: 'Walker Coon Hound',
+    type: 'dog'
+  },
+  {
+    name: 'Piper',
+    age: 1000000,
+    alive: true,
+    breed: 'Terrier',
+    type: 'dog'
+  },
+  {
+    name: 'Veronica',
+    age: 4,
+    alive: true,
+    type: 'cat'
+  },
+  {
+    name: 'Miranda',
+    age: 5,
+    alive: true,
+    type: 'cat'
+  },
+  {
+    name: 'Mirabelle',
+    age: 10,
+    alive: false,
+    type: 'cat'
+  },
+  {
+    name: 'Orange',
+    age: 1,
+    alive: false,
+    type: 'fish'
+  },
+  {
+    name: 'Crawdaddy',
+    age: 3,
+    alive: true,
+    type: 'fish'
+  }
+]
+export default function() {
+  /* Level Extra - Objects */
+  const ex1 = 'Use map to return a list of pet names'
+  const exercise1 = () => {
+    return null // return answer here
+  }
+  const ex2 = 'Use filter to return a list of dead fish'
+  const exercise2 = () => {
+    return null // return answer here
+  }
+
+  const ex3 = 'Use reduce to return the total age of all the cats'
+  const exercise3 = () => {
+    return null // return answer here
+  }
+
+  const ex4 =
+    'Use reduce to return a string of all the dog breeds in the following format: | Dog breed || Dog Breed || Dog Breed |'
+  const exercise4 = () => {
+    return null // return answer here
+  }
+
+  const ex5 =
+    "Return a string of the names of dogs who can legally drink in the US and whose name includes an 'S'. Try once with compose (map, filter, and reduce), and again with only reduce as an extra challenge."
+  const exercise5 = () => {
+    return null // return answer here
+  }
+
+  /* tests to validate exercises go here */
+  test('Exercise 1', assert => {
+    assert.plan(5)
+    assert.same(
+      exercise1(),
+      [
+        'Nuggett',
+        'Thor',
+        'Scrappy',
+        'Spot',
+        'Shyloh',
+        'Piper',
+        'Veronica',
+        'Miranda',
+        'Mirabelle',
+        'Orange',
+        'Crawdaddy'
+      ],
+      ex1
+    )
+    assert.same(
+      exercise2(),
+      [{ name: 'Orange', age: 1, alive: false, type: 'fish' }],
+      ex2
+    )
+    assert.same(exercise3(), 19, ex3)
+    assert.same(
+      exercise4(),
+      '| Golden Retriever | | German Shepherd | | Beagle | | Dachsund | | Walker Coon Hound | | Terrier |',
+      ex4
+    )
+    assert.same(exercise5(), 'Scrappy Spot Shyloh', ex5)
+  })
+}
